cb497e5f80dc5f1cd629fc0dd6e3e2163e3b9be8
ashele1234 <calebashele@gmail.com>
2024-10-30 21:52:21 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,88 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from 'react'
+import { MdCancel, MdMenu } from 'react-icons/md'
 
-export default function Home() {
+
+const Navs=[
+  {
+    id:1,
+     name:"Home"
+  },
+  {
+    id:1,
+     name:"About"
+  },
+  {
+    id:1,
+     name:"Services"
+  },
+  {
+    id:1,
+     name:"News"
+  },
+  {
+    id:1,
+     name:"FAQ"
+  }
+]
+const Page = () => {
+  const[Toggle,setToggle]=useState<boolean>(false)
+  const OnToggle=()=>{
+    setToggle(!Toggle)
+  }
+  const OnCancel=()=>{
+    setToggle(false)
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+   <div className=' flex justify-center items-center flex-col'>
+    <header className='w-[100%] h-[80px] bg-white flex justify-center items-center fixed top-0 z-30 lg:flex max-lg:flex '>
+      <header className='w-[90%] h-[80px] bg-white flex justify-between items-center fixed top-0 z-30'>
+        <div className="font-extrabold text-2xl font-serif">ResumeGrove</div>
+        <div className='flex justify-center items-center gap-10 max-md:hidden'>
+        {
+          Navs.map((el:any)=>(
+            <div className=' font-semibold text-base cursor-pointer hover:underline decoration-4 underline-offset-8 font-serif lg:flex md:hidden max-sm:hidden'
+             key={el.id}>
+              <div>{el.name}</div>
+             </div>
+          ))
+        }
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className='flex gap-4 max-md:hidden max-sm:hidden'>
+          <button className='px-6 py-4 bg-#fff text-[#502ACD] rounded-md border font-semibold hover:bg-[#502acd1a] border-purple-900 font-serif'>Sign in</button>
+          <button className='px-6 py- rounded-md font-semibold bg-[#502ACD] text-white hover:bg-black font-serif'>Register</button>
+        </div>
+        <div className='text-black  sm:block max-sm:flex -mr-4 lg:hidden md:flex flex justify-center items-center'>
+           {!Toggle? <MdMenu className='text-black w-[35px] h-[35px] cursor-pointer' onClick={OnToggle}/>:<MdCancel  className='text-black w-[35px] h-[35px] cursor-pointer'onClick={OnCancel}/>}
+          </div>
+
+      </header>
+    </header>
+   {Toggle && <div className=" gap-4 w-full h-[100vh] bg-black absolute top-0 z-10 mt-[80px] transition-all duration-300 flex justify-start items-start flex-col px-6 cursor-pointer md:hidden lg:hidden">
+    {
+      Navs.map((el:any)=>(
+        <div className='text-white font-semibold mt-8 font-serif' key={el.id}>
+          {el.name}
+        </div>
+      ))
+    }
+    <div className='flex gap-4 flex-col'>
+          <button className='px-48 py-4 bg-slate-900 rounded-md border font-semibold hover:bg-[#502ACD] text-[#fff] font-serif max-sm:px-44 max-md:px-72'>Sign in</button>
+          <button className='px-48 py-4 rounded-md font-semibold bg-[#502ACD] text-white hover:bg-slate-600 font-serif max-sm:px-44 max-md:px-72'>Register</button>
+        </div>
+    </div>}
+    <section className=' sec-1 w-full h-[100vh] bg-slate-800 flex justify-center items-center flex-col gap-2'>
+    <div className=' transition-all duration-300 absolute w-full h-[100vh] top-0 flex flex-col bg-[rgba(0,0,0,0.57)] justify-center items-center max-sm:gap-4'>
+    <h1 className='font-bold text-7xl text-white font-serif text-center max-sm:text-4xl mt-6'>Craft a resume that gets <br /> you hired.</h1>
+    <p className='font-bold text-2xl text-white font-serif text-center max-sm:text-sm max-w-[700px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cumque assumenda,labore cum molestias sint!</p>
+    <div className='flex justify-center items-center gap-2 mt-6'>
+      <button className='px-10 py-3 bg-white font-serif rounded-md drop-shadow-2xl max-sm:px-6'>CREATE CV</button>
+      <button className='px-10 py-3 bg-[#502ACD] text-white font-serif rounded-md drop-shadow-2xl max-sm:px-6'>LEARN MORE</button>
     </div>
-  );
+    </div>
+    </section>
+   </div>
+  )
 }
+
+export default Page
